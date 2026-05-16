f1b5d3b47b7379eaa0365b2220ac0913177181ea
www-icloud-net <systemprojects7@gmail.com>
2026-05-16 22:18:24 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'land-dunamis-cms-2026-05-16-smart-ops';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './church-icon-192.png',
+  './church-icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (request.mode === 'navigate') {
+    event.respondWith(fetch(request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+      return response;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
+  if (url.origin === self.location.origin) {
+    event.respondWith(caches.match(request).then(cached => cached || fetch(request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+      return response;
+    }).catch(() => cached)));
+  }
+});
